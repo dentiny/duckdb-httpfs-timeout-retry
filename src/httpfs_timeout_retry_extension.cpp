7563b092e31d62354eab18ecd266ce446ee29d6f
dentiny <dentinyhao@gmail.com>
2026-02-15 01:45:22 -0800

    Provide extension setting (#3)

--- src/httpfs_timeout_retry_extension.cpp
+++ src/httpfs_timeout_retry_extension.cpp
@@ -2,40 +2,62 @@
 
 #include "httpfs_timeout_retry_extension.hpp"
 #include "duckdb.hpp"
-#include "duckdb/common/exception.hpp"
-#include "duckdb/function/scalar_function.hpp"
-#include <duckdb/parser/parsed_data/create_scalar_function_info.hpp>
-
-// OpenSSL linked through vcpkg
-#include <openssl/opensslv.h>
+#include "duckdb/common/http_util.hpp"
 
 namespace duckdb {
 
-inline void QuackScalarFun(DataChunk &args, ExpressionState &state, Vector &result) {
-	auto &name_vector = args.data[0];
-	UnaryExecutor::Execute<string_t, string_t>(name_vector, result, args.size(), [&](string_t name) {
-		return StringVector::AddString(result, "Quack " + name.GetString() + " 🐥");
-	});
+namespace {
+
+// Default values matching httpfs extension for compatibility
+constexpr uint64_t DEFAULT_TIMEOUT_MS = HTTPParams::DEFAULT_TIMEOUT_SECONDS * 1000;
+constexpr uint64_t DEFAULT_RETRIES = HTTPParams::DEFAULT_RETRIES;
+constexpr uint64_t DEFAULT_RETRY_WAIT_MS = HTTPParams::DEFAULT_RETRY_WAIT_MS;
+constexpr float DEFAULT_RETRY_BACKOFF = HTTPParams::DEFAULT_RETRY_BACKOFF;
+
+void LoadInternal(ExtensionLoader &loader) {
+	auto &instance = loader.GetDatabaseInstance();
+	auto &config = DBConfig::GetConfig(instance);
+
+	// Timeout settings for different HTTP operations (in milliseconds)
+	config.AddExtensionOption("httpfs_timeout_open", "Timeout for opening files (in milliseconds)", LogicalType::UBIGINT,
+	                          Value::UBIGINT(DEFAULT_TIMEOUT_MS));
+	config.AddExtensionOption("httpfs_timeout_read", "Timeout for reading files (in milliseconds)", LogicalType::UBIGINT,
+	                          Value::UBIGINT(DEFAULT_TIMEOUT_MS));
+	config.AddExtensionOption("httpfs_timeout_write", "Timeout for writing files (in milliseconds)", LogicalType::UBIGINT,
+	                          Value::UBIGINT(DEFAULT_TIMEOUT_MS));
+	config.AddExtensionOption("httpfs_timeout_list", "Timeout for listing directories (in milliseconds)", LogicalType::UBIGINT,
+	                          Value::UBIGINT(DEFAULT_TIMEOUT_MS));
+	config.AddExtensionOption("httpfs_timeout_delete", "Timeout for deleting files (in milliseconds)", LogicalType::UBIGINT,
+	                          Value::UBIGINT(DEFAULT_TIMEOUT_MS));
+	config.AddExtensionOption("httpfs_timeout_connect", "Timeout for establishing connections (in milliseconds)",
+	                          LogicalType::UBIGINT, Value::UBIGINT(DEFAULT_TIMEOUT_MS));
+
+	// Retry settings for different HTTP operations
+	config.AddExtensionOption("httpfs_retries_open", "Maximum number of retries for opening files", LogicalType::UBIGINT,
+	                          Value::UBIGINT(DEFAULT_RETRIES));
+	config.AddExtensionOption("httpfs_retries_read", "Maximum number of retries for reading files", LogicalType::UBIGINT,
+	                          Value::UBIGINT(DEFAULT_RETRIES));
+	config.AddExtensionOption("httpfs_retries_write", "Maximum number of retries for writing files", LogicalType::UBIGINT,
+	                          Value::UBIGINT(DEFAULT_RETRIES));
+	config.AddExtensionOption("httpfs_retries_list", "Maximum number of retries for listing directories", LogicalType::UBIGINT,
+	                          Value::UBIGINT(DEFAULT_RETRIES));
+	config.AddExtensionOption("httpfs_retries_delete", "Maximum number of retries for deleting files", LogicalType::UBIGINT,
+	                          Value::UBIGINT(DEFAULT_RETRIES));
+	config.AddExtensionOption("httpfs_retries_connect", "Maximum number of retries for establishing connections", LogicalType::UBIGINT,
+	                          Value::UBIGINT(DEFAULT_RETRIES));
+
+	// Global retry behavior settings
+	config.AddExtensionOption("httpfs_retry_wait_ms", "Initial wait time between retries (in milliseconds)",
+	                          LogicalType::UBIGINT, Value::UBIGINT(DEFAULT_RETRY_WAIT_MS));
+	config.AddExtensionOption("httpfs_retry_backoff", "Backoff factor for exponentially increasing retry wait time",
+	                          LogicalType::FLOAT, Value::FLOAT(DEFAULT_RETRY_BACKOFF));
+	config.AddExtensionOption("httpfs_retry_on_timeout", "Whether to retry on timeout errors", LogicalType::BOOLEAN,
+	                          Value::BOOLEAN(true));
+	config.AddExtensionOption("httpfs_retry_on_connection_error", "Whether to retry on connection errors",
+	                          LogicalType::BOOLEAN, Value::BOOLEAN(true));
 }
 
-inline void QuackOpenSSLVersionScalarFun(DataChunk &args, ExpressionState &state, Vector &result) {
-	auto &name_vector = args.data[0];
-	UnaryExecutor::Execute<string_t, string_t>(name_vector, result, args.size(), [&](string_t name) {
-		return StringVector::AddString(result, "Quack " + name.GetString() + ", my linked OpenSSL version is " +
-		                                           OPENSSL_VERSION_TEXT);
-	});
-}
-
-static void LoadInternal(ExtensionLoader &loader) {
-	// Register a scalar function
-	auto quack_scalar_function = ScalarFunction("quack", {LogicalType::VARCHAR}, LogicalType::VARCHAR, QuackScalarFun);
-	loader.RegisterFunction(quack_scalar_function);
-
-	// Register another scalar function
-	auto quack_openssl_version_scalar_function = ScalarFunction("quack_openssl_version", {LogicalType::VARCHAR},
-	                                                            LogicalType::VARCHAR, QuackOpenSSLVersionScalarFun);
-	loader.RegisterFunction(quack_openssl_version_scalar_function);
-}
+} // namespace
 
 void HttpfsTimeoutRetryExtension::Load(ExtensionLoader &loader) {
 	LoadInternal(loader);
